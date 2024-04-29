f3036ce4027deaf4692426cb835972f7cfc69c66
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-04-29 13:33:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [buttonText, setButtonText] = useState('Press for Surprise'); // Corrected initial text
+
+  const images = [
+    'dogs1.jpg', 'dogs2.jpg', 'dogs3.jpg', 'dogs4.jpg', 'dogs5.jpg',
+    'dogs6.jpg'
+  ];
+
+  const handleClick = () => {
+    if (buttonText === 'Press for Surprise') { // Make sure this matches exactly the initial state
+      setButtonText('Next Pup');
+    }
+    setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen p-4">
+      <div className="flex flex-col items-center"> {/* Ensures images and button are aligned vertically */}
+        {buttonText !== 'Press for Surprise' && (
+          <img src={images[imageIndex]} alt="Cute dog" className="mb-4 max-w-md rounded-lg shadow-lg" /> // Adjusted max width and added rounded edges
+        )}
+        <button className="bg-gradient-to-r from-violet-200 to-pink-200 text-white font-bold py-2 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105" onClick={handleClick}>
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // paths to all of your components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // Optionally, you can enable JIT mode by adding the `mode` property:
+  // mode: 'jit',
+}
